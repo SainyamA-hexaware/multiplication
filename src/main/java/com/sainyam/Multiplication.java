34df211a4fae2eb6730512ae61010dcb0588742a
sainyam_agrawal <SainyamA@hexaware.com>
2018-01-24 12:04:55 +0530

    first commit

---
+++ src/main/java/com/sainyam/Multiplication.java
@@ -0,0 +1,13 @@
+package com.sainyam;
+public class Multiplication
+{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        System.out.println(Multi(a,b));
+    }
+    public static int Multi (int a, int b)
+    {
+        return a*b;
+    }
+}
